007258ffc7a56328007e55c90ff51f0c64dfe84a
Ciaron Linstead <ciaron.linstead@gmail.com>
2019-03-29 13:58:58 +0100

    Add new "Thinking In React" tutorial code

---
+++ thinking-in-react/src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class SearchBar extends Component {
+  render() {
+
+  }
+}
+class ProductTable extends Component {
+  render() {
+
+  }
+}
+class ProductCategoryRow extends Component {
+  render() {
+
+  }
+}
+class ProductRow extends Component {
+  render() {
+
+  }  
+}
+
+
+class FilterableProductTable extends Component {
+  render() {
+    return <table>test</table>
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <FilterableProductTable />
+      </div>
+    );
+  }
+}
+
+export default App;
